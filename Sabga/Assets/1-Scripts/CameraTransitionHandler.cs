a52c712f0fd479507c1f18d350347c18182e2f3d
SebastianEsco <82065985+Adept-KeyCap@users.noreply.github.com>
2024-10-16 12:54:36 -0500

    Camera In and Out Transition

---
+++ Sabga/Assets/1-Scripts/CameraTransitionHandler.cs
@@ -0,0 +1,46 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CameraTransitionHandler : MonoBehaviour
+{
+    [SerializeField] private Image transitionPanel;
+    [SerializeField] private Color solidColor;
+    [SerializeField] private float transitionDuration;
+
+    public UnityEvent transparent2Color_End;
+    public UnityEvent color2Transparent_End;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color2Transparent();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Transparent2Color()
+    {
+        transitionPanel.DOColor(solidColor, transitionDuration).SetEase(Ease.InQuad).OnComplete(() =>
+        {
+            color2Transparent_End.Invoke();
+            DOTween.Kill(transitionPanel);
+        });
+    }
+
+    public void Color2Transparent()
+    {
+        transitionPanel.DOColor(Color.clear, transitionDuration).SetEase(Ease.InQuad).OnComplete(() =>
+        {
+            transparent2Color_End.Invoke();
+            DOTween.Kill(transitionPanel);
+        });
+    }
+}
