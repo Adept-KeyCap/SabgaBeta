ee636f3d75fd5cbf7671dced1a5a94e006327976
Adept-KeyCap <82065985+Adept-KeyCap@users.noreply.github.com>
2024-10-19 21:34:19 -0500

    New Project

---
+++ Sabga_U6/Assets/1-Scripts/DepthVisualsHandler.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+using Oculus.Interaction;
+
+public class DepthVisualsHandler : MonoBehaviour
+{
+    // Reference to the URP Asset
+    private UniversalRenderPipelineAsset urpAsset;
+
+    void Start()
+    {
+        // Get the active URP asset (Pipeline settings)
+        urpAsset = (UniversalRenderPipelineAsset)GraphicsSettings.currentRenderPipeline;
+        DisableFog();
+    }
+
+    // Enable fog
+    public void EnableFog()
+    {
+        if (urpAsset != null)
+        {
+            urpAsset.supportsCameraDepthTexture = true; // Fog requires depth texture to be enabled
+            Shader.EnableKeyword("_FOG_EXP2"); // Enable fog for linear fog mode
+            RenderSettings.fog = true;
+            Debug.Log("Fog Enabled");
+        }
+    }
+
+    // Disable fog
+    public void DisableFog()
+    {
+        if (urpAsset != null)
+        {
+            RenderSettings.fog = false;
+            Shader.DisableKeyword("_FOG_LINEAR"); // Disable fog
+            Debug.Log("Fog Disabled");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != null && other.gameObject.CompareTag("Player"))
+        {
+            EnableFog();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other != null && other.gameObject.CompareTag("Player"))
+        {
+            DisableFog();
+        }
+    }
+}
